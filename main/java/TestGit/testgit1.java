c6a9cdf07d7a4ddf1481f73c6ab24d200dea740b
chj <534680369@qq.com>
2023-03-22 18:18:49 +0800

    third.xml

--- main/java/TestGit/testgit1.java
+++ main/java/TestGit/testgit1.java
@@ -3,5 +3,7 @@ package TestGit;
 public class testgit1 {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!2");
+
     }
 }
