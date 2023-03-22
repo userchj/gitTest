87dab0023524848c7e80b57b4290a3cbe0dc9544
chj <534680369@qq.com>
2023-03-22 18:02:30 +0800

    pom.xml

---
+++ main/java/TestGit/testgit1.java
@@ -0,0 +1,7 @@
+package TestGit;
+
+public class testgit1 {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ main/java/TestGit/tsetgit2.java
@@ -0,0 +1,7 @@
+package TestGit;
+
+public class tsetgit2 {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
